01bcb4ab3dd3a6b82de0f3588d8901d7e3608959
Sorin Marian <sorin@clevertaxi.com>
2020-02-03 18:02:41 +0200

    fix core.py file indentation

--- src/config/core.py
+++ src/config/core.py
@@ -1,25 +1,26 @@
 url = {
-	"base" : "https://www.pizzahutdelivery.ro"
+    "base" : "https://www.pizzahutdelivery.ro"
 }
 pizzas = {
-	"types": {
-		"pizza-margherita",
-		"pizza-quattro-stagioni",
-		"pizza-rodeo",
-		"pizza-roma",
-		"pizza-california",
-		"pizza-europeana"
-	},
-	"sizes": {
-		"size_small",
-		"size_medium",
-		"size_large"
-	},
-	"bases" : {
-		"base_trad",
-		"base_italian",
-		"base_pan",
-		"base_crust",
-		"base_chbit"
-	}
+    "types": {
+        "pizza-margherita",
+        "pizza-quattro-stagioni",
+        "pizza-rodeo",
+        "pizza-roma",
+        "pizza-california",
+        "pizza-europeana"
+    },
+    "sizes": {
+        "size_small",
+        "size_medium",
+        "size_large"
+
+    },
+    "bases" : {
+        "base_trad",
+        "base_italian",
+        "base_pan",
+        "base_crust",
+        "base_chbit"
+    }
 }
