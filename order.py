78e86598cbdb70d86bd1197c0242bac4b04dfb01
Sorin Marian <marian.sorin@yahoo.com>
2020-03-31 12:53:43 +0300

    decouple log method from OrderPizza class

--- order.py
+++ order.py
@@ -1,9 +1,9 @@
 from src.Config import Config
 from src.OrderPizza import OrderPizza
 import src.config.core as configFile
-
+from src.Logger import Logger
 
 config = Config(configFile)
-order = OrderPizza(config)
-
+logger = Logger()
+order = OrderPizza(config, logger)
 order.acceptCookies()
--- src/OrderPizza.py
+++ src/OrderPizza.py
@@ -2,27 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+import time
 
 class OrderPizza:
-    logCounter = 0
+    logger = None
 
-    def __init__(self, config):
+    def __init__(self, config, logger):
         self.config = config
+        self.logger = logger
         self.initDriver()
-
-    def log(self, text):
-        self.logCounter += 1
-        print(str(self.logCounter) + ". " + text)
 
     def initDriver(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('headless')
+        #chrome_options.add_argument('headless')
         self.driver = webdriver.Chrome(options=chrome_options)
 
     def acceptCookies(self):
         self.driver.get(self.config.getMyAccountUrl())
-        self.log("Open pizzahutdelivery.ro")
+        time.sleep(5)
+        self.logger.p("Open pizzahutdelivery.ro")
         acceptCookies = self.driver.find_element_by_css_selector('a#CybotCookiebotDialogBodyLevelButtonAccept')
         acceptCookies.click()
-        self.log("Accept cookies")
+        self.logger.p("Accept cookies")
 
